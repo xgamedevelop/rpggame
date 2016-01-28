b46d2299a2287b682b02d9bb95ea90b85ba98cd2
Gary Ascuy <gary.ascuy@gmail.com>
2016-01-27 23:32:25 -0400

    Adding basic UI controller

---
+++ game/Assets/gui/home/GuiHome.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiHome : MonoBehaviour
+{
+    void OnGUI ()
+    {
+        var W = Screen.width;
+        var H = Screen.height;
+        int w = 200, h = 100, space = 20;
+        int y = H - h - space;
+
+        if (GUI.Button (new Rect (space, y, w, h), "Exit")) {
+            Application.Quit ();
+        }
+        if (GUI.Button (new Rect (W - w - space, y, w, h), "Play")) {
+            Debug.Log ("Play");
+        }
+    }
+}
