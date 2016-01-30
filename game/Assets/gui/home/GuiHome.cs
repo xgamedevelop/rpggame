833331e1785ea4518231107f8e9412846c27bfe5
Gary Ascuy <gary.ascuy@gmail.com>
2016-01-30 18:07:58 -0400

    Adding snowy in home

--- game/Assets/gui/home/GuiHome.cs
+++ game/Assets/gui/home/GuiHome.cs
@@ -3,17 +3,48 @@ using System.Collections;
 
 public class GuiHome : MonoBehaviour
 {
+    public GameObject Prefab;
+    public int Count = 10;
+
     private GameObject bg;
+    private GameObject[] snowflakes;
+
+    void MoveToUp (GameObject snowflake)
+    {
+        var range = Camera.main.aspect * 5f;
+        var size = Random.Range (0.5f, 1.5f);
+        var position = new Vector2 (Random.Range (-range, range), Random.Range (7f, 14f));
+
+        // transform
+        snowflake.transform.localScale = new Vector2 (size, size);
+        snowflake.transform.localPosition = position;
+
+        // gravity and velocity
+        var rigidbody = snowflake.GetComponent<Rigidbody2D> () as Rigidbody2D;
+        rigidbody.gravityScale = 0.01f + ((size - 0.5f) * 0.05f);
+        rigidbody.velocity = Vector3.zero;
+    }
 
     void Start ()
     {
         bg = GameObject.Find ("bg");
+        snowflakes = new GameObject[Count];
+        for (int i = 0; i < snowflakes.Length; ++i) {
+            snowflakes [i] = Instantiate (Prefab, new Vector2 (0, 0), Quaternion.identity) as GameObject;
+            MoveToUp (snowflakes [i]);
+        }
     }
 
     void Update ()
     {
         var scale = new Vector2 (Camera.main.aspect * 2f, 3.3f);
         bg.transform.localScale = scale;
+
+        foreach (var snowflake in snowflakes) {
+            if (snowflake.transform.localPosition.y < -6) {
+                MoveToUp (snowflake);
+            }
+        }
     }
 
     void OnGUI ()
