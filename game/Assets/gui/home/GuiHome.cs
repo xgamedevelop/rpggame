147ef08b861f66d39a09962137247dcf704d74ee
Gary Ascuy <gary.ascuy@gmail.com>
2016-01-30 16:44:55 -0400

    Adding scale background

--- game/Assets/gui/home/GuiHome.cs
+++ game/Assets/gui/home/GuiHome.cs
@@ -3,10 +3,24 @@ using System.Collections;
 
 public class GuiHome : MonoBehaviour
 {
+    private GameObject bg;
+
+    void Start ()
+    {
+        bg = GameObject.Find ("bg");
+    }
+
+    void Update ()
+    {
+        var scale = new Vector2 (Camera.main.aspect * 2f, 3.3f);
+        bg.transform.localScale = scale;
+    }
+
     void OnGUI ()
     {
         var W = Screen.width;
         var H = Screen.height;
+
         int w = 200, h = 100, space = 20;
         int y = H - h - space;
 
