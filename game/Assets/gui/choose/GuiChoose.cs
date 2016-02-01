a54f0153c6195c669ed1b887c1a04344a957406f
Gary Ascuy <gary.ascuy@gmail.com>
2016-02-01 14:39:08 -0400

    Adding exit button

---
+++ game/Assets/gui/choose/GuiChoose.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiChoose : MonoBehaviour
+{
+    public Texture2D ExitIcon;
+    public GUISkin EmptySkin;
+
+    void OnGUI ()
+    {
+        GUI.skin = EmptySkin;
+        var H = Screen.height;
+
+        int h = (int)(H * 0.15), w = h * 2, space = 10;
+        int y = H - h - space;
+
+        // exit
+        if (GUI.Button (new Rect (2 * space, y, w, h), ExitIcon)) {
+            Application.Quit ();
+        }
+    }
+}
