7ce4f307e091fd68ad4043c361207037879a1ca6
Gary Ascuy <gary.ascuy@gmail.com>
2016-02-27 22:08:54 -0400

    Adding buttons and Joystick

---
+++ game/Assets/levels/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+	void Start () {
+	}
+	
+	void FixedUpdate () {
+        var x = CrossPlatformInputManager.GetAxis ("Horizontal");
+        var y = CrossPlatformInputManager.GetAxis ("Vertical");
+        var move = new Vector2 (-x, -y) * 2;
+        transform.localPosition = move;
+
+        var a = CrossPlatformInputManager.GetButton ("A");
+        var b = CrossPlatformInputManager.GetButton ("B");
+        Debug.LogFormat ("{0}, {1}", a, b);
+	}
+}
